f0b294f704e510d4eb64ee7eb4ca24a2ac135c9e
Diogo Martins <diogo.olivins@gmail.com>
2017-09-25 17:28:51 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/diogomartins/cursomc/services/ClienteService.java
@@ -0,0 +1,27 @@
+package com.diogomartins.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.diogomartins.cursomc.domain.Cliente;
+import com.diogomartins.cursomc.repositories.ClienteRepository;
+import com.diogomartins.cursomc.services.exception.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Cliente obj = repo.findOne(id);
+		
+		if(obj == null) {
+			
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + 
+					", Tipo: " + Cliente.class.getName());
+			
+		}
+		return obj;
+	}
+}
