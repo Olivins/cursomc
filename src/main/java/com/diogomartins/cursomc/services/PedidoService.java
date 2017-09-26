c809bad3cf182dab7bb9f0567fcce8d858e6e740
Diogo Martins <diogo.olivins@gmail.com>
2017-09-26 11:54:19 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/diogomartins/cursomc/services/PedidoService.java
@@ -0,0 +1,27 @@
+package com.diogomartins.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.diogomartins.cursomc.domain.Pedido;
+import com.diogomartins.cursomc.repositories.PedidoRepository;
+import com.diogomartins.cursomc.services.exception.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Pedido obj = repo.findOne(id);
+		
+		if(obj == null) {
+			
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + 
+					", Tipo: " + Pedido.class.getName());
+			
+		}
+		return obj;
+	}
+}
