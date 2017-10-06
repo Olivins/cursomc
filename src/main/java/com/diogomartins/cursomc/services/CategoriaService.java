e7e5cb5ab1a9dff4c206efb808a608ab56513bcf
Diogo Martins <diogo.olivins@gmail.com>
2017-10-05 22:03:05 -0300

    Listando todas categorias com DTO

--- src/main/java/com/diogomartins/cursomc/services/CategoriaService.java
+++ src/main/java/com/diogomartins/cursomc/services/CategoriaService.java
@@ -1,5 +1,7 @@
 package com.diogomartins.cursomc.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -11,39 +13,43 @@ import com.diogomartins.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria find(Integer id) {
 		Categoria obj = repo.findOne(id);
-		
-		if(obj == null) {
-			
-			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + 
-					", Tipo: " + Categoria.class.getName());
-			
+
+		if (obj == null) {
+
+			throw new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName());
+
 		}
 		return obj;
 	}
-	
+
 	public void insert(Categoria obj) {
 		obj.setId(null);
 		repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
-		
+
 		return repo.save(obj);
 	}
+
 	public void delete(Integer id) {
-		 	find(id);
-		 	try {
-		 		repo.delete(id);
-		 	}
-		 	catch (DataIntegrityViolationException e) {
-		 		throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
-		 	}
-		 }
+		find(id);
+		try {
+			repo.delete(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
+	}
+
+	public List<Categoria> findAll() {
+		return repo.findAll();
+	}
 }
