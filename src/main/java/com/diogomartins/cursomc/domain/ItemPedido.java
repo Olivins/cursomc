6104ad11b03506870abeb1ab555235b7a90c1394
Diogo Martins <diogo.olivins@gmail.com>
2017-11-09 21:04:00 -0200

    Implementando toString de Pedido

--- src/main/java/com/diogomartins/cursomc/domain/ItemPedido.java
+++ src/main/java/com/diogomartins/cursomc/domain/ItemPedido.java
@@ -1,6 +1,8 @@
 package com.diogomartins.cursomc.domain;
 
 import java.io.Serializable;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
@@ -32,7 +34,6 @@ public class ItemPedido implements Serializable {
 		this.preco = preço;
 	}
 
-
 	public double getSubTotal() {
 		return (preco - desconto) * quantidade;
 	}
@@ -41,13 +42,15 @@ public class ItemPedido implements Serializable {
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
-	
+
 	public void setPedido(Pedido pedido) {
 		id.setPedido(pedido);
 	}
+
 	public Produto getProduto() {
 		return id.getProduto();
 	}
+
 	public void setProduto(Produto produto) {
 		id.setProduto(produto);
 	}
@@ -107,6 +110,21 @@ public class ItemPedido implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	@Override
+	public String toString() {
+		NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+		StringBuilder builder = new StringBuilder();
+		builder.append(getProduto().getNome());
+		builder.append(", Qte: ");
+		builder.append(getQuantidade());
+		builder.append(", Preço unitário: ");
+		builder.append(nf.format(getPreco()));
+		builder.append(", Subtotal: ");
+		builder.append(nf.format(getSubTotal()));
+		builder.append("\n");
+		return builder.toString();
 	}
 
 }
--- src/main/java/com/diogomartins/cursomc/services/PedidoService.java
+++ src/main/java/com/diogomartins/cursomc/services/PedidoService.java
@@ -9,6 +9,7 @@ import com.diogomartins.cursomc.domain.ItemPedido;
 import com.diogomartins.cursomc.domain.PagamentoComBoleto;
 import com.diogomartins.cursomc.domain.Pedido;
 import com.diogomartins.cursomc.domain.enums.EstadoPagamento;
+import com.diogomartins.cursomc.repositories.ClienteRepository;
 import com.diogomartins.cursomc.repositories.ItemPedidoRepository;
 import com.diogomartins.cursomc.repositories.PagamentoRepository;
 import com.diogomartins.cursomc.repositories.PedidoRepository;
@@ -32,6 +33,9 @@ public class PedidoService {
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
 
+	@Autowired
+	private ClienteRepository clienteRepository;
+
 	public Pedido find(Integer id) {
 		Pedido obj = repo.findOne(id);
 
@@ -46,6 +50,7 @@ public class PedidoService {
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
+		obj.setCliente(clienteRepository.findOne(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		if (obj.getPagamento() instanceof PagamentoComBoleto) {
@@ -57,9 +62,12 @@ public class PedidoService {
 		for (ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
 			ip.setPreco(produtoRepository.findOne(ip.getProduto().getId()).getPreco());
+			ip.setProduto(produtoRepository.findOne(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.save(obj.getItens());
+		System.out.println(obj);
 		return obj;
 	}
 }
