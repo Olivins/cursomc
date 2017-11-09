73aa3b2a12a671f7f156d8bee1f184060cd27e73
Diogo Martins <diogo.olivins@gmail.com>
2017-11-09 21:31:45 -0200

    MockEmailService com Logger. Padroes Strategy e Template Method

--- src/main/java/com/diogomartins/cursomc/config/TestConfig.java
+++ src/main/java/com/diogomartins/cursomc/config/TestConfig.java
@@ -8,18 +8,25 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.diogomartins.cursomc.services.DBService;
+import com.diogomartins.cursomc.services.EmailService;
+import com.diogomartins.cursomc.services.MockEmailService;
 
 @Configuration
 @Profile("test")
 public class TestConfig {
-	
+
 	@Autowired
-	 	private DBService dbService;
-	 	
-	 	@Bean
-	 	public boolean instantiateDatabase() throws ParseException {
-	 		dbService.instantiateTestDatabase();
-	 		return true;
-	 	}
+	private DBService dbService;
+
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+
+	@Bean
+	public EmailService emailService() {
+		return new MockEmailService();
+	}
 
 }
