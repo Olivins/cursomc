6b915bb065afd22280fbd954c2458a6a0c80654e
Diogo Martins <diogo.olivins@gmail.com>
2017-11-23 20:02:35 -0300

    Endpoint - Inserindo um novo produto com POST

--- src/main/java/com/diogomartins/cursomc/resources/ProdutoResource.java
+++ src/main/java/com/diogomartins/cursomc/resources/ProdutoResource.java
@@ -1,15 +1,21 @@
 package com.diogomartins.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.diogomartins.cursomc.domain.Produto;
 import com.diogomartins.cursomc.dto.ProdutoDTO;
@@ -17,30 +23,42 @@ import com.diogomartins.cursomc.resources.utils.URL;
 import com.diogomartins.cursomc.services.ProdutoService;
 
 @RestController
-@RequestMapping(value="/produtos")
+@RequestMapping(value = "/produtos")
 public class ProdutoResource {
-	
+
 	@Autowired
 	private ProdutoService service;
-	
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Produto> find(@PathVariable Integer id) {
 		Produto obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
+
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity<Page<ProdutoDTO>> findPage(
-			@RequestParam(value="nome", defaultValue="") String nome, 
-			@RequestParam(value="categorias", defaultValue="") String categorias, 
-			@RequestParam(value="page", defaultValue="0") Integer page, 
-			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, 
-			@RequestParam(value="orderBy", defaultValue="nome") String orderBy, 
-			@RequestParam(value="direction", defaultValue="ASC") String direction) {
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> inserir(@Valid @RequestBody ProdutoDTO objDto) {
+		Produto obj = service.fromDTO(objDto);
+		service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+		
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<Page<ProdutoDTO>> findPage(@RequestParam(value = "nome", defaultValue = "") String nome,
+			@RequestParam(value = "categorias", defaultValue = "") String categorias,
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
 		String nomeDecoded = URL.decodeParam(nome);
 		List<Integer> ids = URL.decodeIntList(categorias);
 		Page<Produto> list = service.search(nomeDecoded, ids, page, linesPerPage, orderBy, direction);
-		Page<ProdutoDTO> listDto = list.map(obj -> new ProdutoDTO(obj));  
+		Page<ProdutoDTO> listDto = list.map(obj -> new ProdutoDTO(obj));
 		return ResponseEntity.ok().body(listDto);
 	}
 
--- src/main/java/com/diogomartins/cursomc/services/ProdutoService.java
+++ src/main/java/com/diogomartins/cursomc/services/ProdutoService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import com.diogomartins.cursomc.domain.Categoria;
 import com.diogomartins.cursomc.domain.Produto;
+import com.diogomartins.cursomc.dto.ProdutoDTO;
 import com.diogomartins.cursomc.repositories.CategoriaRepository;
 import com.diogomartins.cursomc.repositories.ProdutoRepository;
 import com.diogomartins.cursomc.services.exception.ObjectNotFoundException;
@@ -31,10 +32,19 @@ public class ProdutoService {
 		}
 		return obj;
 	}
+	
+	public Produto insert(Produto obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
 
 	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		List<Categoria> categorias = categoriaRepository.findAll(ids);
 		return repo.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);	
+	}
+	
+	public Produto fromDTO(ProdutoDTO objDto) {
+		return new Produto(objDto.getId(), objDto.getNome(), objDto.getPreco());
 	}
 }
